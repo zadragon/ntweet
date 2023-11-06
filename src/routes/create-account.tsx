6c4b96063c7eefc2a3bfa140888bf12d11be4560
zadragon <mase0804@naver.com>
2023-11-07 00:22:02 +0900

    update

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,144 +1,69 @@
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import { styled } from "styled-components";
 import { auth } from "../firebase";
 import { Link, useNavigate } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
+import { Error, Form, Input, Switcher, Title, Wrapper } from "../components/auth-components";
 
 export default function CreateAccount() {
-  const navigate = useNavigate();
-  const [isLoading, setLoading] = useState(false);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { name, value },
-    } = e;
-    if (name === "name") {
-      setName(value);
-    } else if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-  };
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setError("");
-    if (isLoading || name === "" || email === "" || password === "") return;
+	const navigate = useNavigate();
+	const [isLoading, setLoading] = useState(false);
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const {
+			target: { name, value },
+		} = e;
+		if (name === "name") {
+			setName(value);
+		} else if (name === "email") {
+			setEmail(value);
+		} else if (name === "password") {
+			setPassword(value);
+		}
+	};
+	const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setError("");
+		if (isLoading || name === "" || email === "" || password === "") return;
 
-    try {
-      setLoading(true);
-      const credetials = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
+		try {
+			setLoading(true);
+			const credetials = await createUserWithEmailAndPassword(auth, email, password);
 
-      console.log(credetials.user);
-      await updateProfile(credetials.user, {
-        displayName: name,
-      });
+			console.log(credetials.user);
+			await updateProfile(credetials.user, {
+				displayName: name,
+			});
 
-      navigate("/");
-    } catch (e) {
-      console.log(e);
-      if (e instanceof FirebaseError) {
-        setError(e.message);
-      }
+			navigate("/");
+		} catch (e) {
+			console.log(e);
+			if (e instanceof FirebaseError) {
+				setError(e.message);
+			}
 
-      // setError
-    } finally {
-      setLoading(false);
-    }
-  };
-  return (
-    <Wrapper>
-      <Title>Join 𝕏</Title>
-      <Form onSubmit={onSubmit}>
-        <Input
-          onChange={onChange}
-          name="name"
-          value={name}
-          placeholder="Name"
-          type="text"
-          required
-        />
-        <Input
-          onChange={onChange}
-          name="email"
-          value={email}
-          placeholder="Email"
-          type="email"
-          required
-        />
-        <Input
-          onChange={onChange}
-          value={password}
-          name="password"
-          placeholder="Password"
-          type="password"
-          required
-        />
-        <Input
-          type="submit"
-          value={isLoading ? "Loading..." : "Create Account"}
-        />
-      </Form>
-      {error !== "" ? <Error>{error}</Error> : null}
-      <Swithcer>
-        이미 계정이 있으신가요? <Link to="login">login &rarr;</Link>
-      </Swithcer>
-    </Wrapper>
-  );
+			// setError
+		} finally {
+			setLoading(false);
+		}
+	};
+	return (
+		<Wrapper>
+			<Title>Join 𝕏</Title>
+			<Form onSubmit={onSubmit}>
+				<Input onChange={onChange} name="name" value={name} placeholder="Name" type="text" required />
+				<Input onChange={onChange} name="email" value={email} placeholder="Email" type="email" required />
+				<Input onChange={onChange} value={password} name="password" placeholder="Password" type="password" required />
+				<Input type="submit" value={isLoading ? "Loading..." : "Create Account"} />
+			</Form>
+			{error !== "" ? <Error>{error}</Error> : null}
+			<Switcher>
+				이미 계정이 있으신가요? <Link to="/login">login &rarr;</Link>
+			</Switcher>
+			<GithubButton />
+		</Wrapper>
+	);
 }
-
-const Swithcer = styled.span`
-  margin-top: 20px;
-  a {
-    color: #1d9bf0;
-  }
-`;
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  margin-bottom: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type="submit"] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
