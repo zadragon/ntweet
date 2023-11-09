961df02f40666954ef65ae23fa9f2e49b9b40c3e
zadragon <mase0804@naver.com>
2023-11-09 19:03:47 +0900

    update

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,8 @@
+import { addDoc, collection, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import styled from "styled-components";
+import { auth, db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const PostTweetForm = () => {
   const [isLoading, setLoading] = useState(false);
@@ -12,13 +15,57 @@ const PostTweetForm = () => {
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
+
     if (files && files.length === 1) {
-      setFile(files[0]);
+      let maxSize = 2 * 1024 * 1024; //* 2MB 사이즈 제한
+      let fileSize = files[0].size; //업로드한 파일용량
+      console.log(maxSize);
+      if (fileSize > maxSize) {
+        alert("파일용량은 2MB이하로 업로드해주세요");
+        setFile(null);
+      } else {
+        setFile(files[0]);
+      }
     }
   };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (!user || isLoading || tweet === "" || tweet.length > 180) return;
+
+    try {
+      setLoading(true);
+      const doc = await addDoc(collection(db, "tweets"), {
+        tweet,
+        createdAt: Date.now(),
+        usename: user.displayName || "Anonymous",
+        userId: user.uid,
+      });
+      if (file) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`
+        );
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc, {
+          photo: url,
+        });
+        setTweet("");
+        setFile(null);
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <TextArea
+        required
         rows={5}
         maxLength={180}
         onChange={onChange}
@@ -33,7 +80,7 @@ const PostTweetForm = () => {
         id="file"
         accept="image/*"
       />
-      <SubmitBtn type="submit" value={isLoading ? "Posting..." : "제출!"} />
+      <SubmitBtn type="submit" value={isLoading ? "Posting..." : "제출"} />
     </Form>
   );
 };
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyBL0FSxm6hKVtxhj_0jxIs5jT8QiyxhYck",
   authDomain: "nwitter-92e67.firebaseapp.com",
@@ -13,3 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
