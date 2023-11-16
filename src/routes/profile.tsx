50ba52df168f4b6fa327dc3132a141f9d443f53e
zadragon <mase0804@naver.com>
2023-11-16 18:01:37 +0900

    udpate

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,16 +1,63 @@
-import { useState } from "react";
+import { updateProfile } from "firebase/auth";
+import { collection, getDocs, limit, query, where } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { auth } from "../firebase";
+import { auth, db, storage } from "../firebase";
 
 const Profile = () => {
   const user = auth.currentUser;
   const [avatar, setAvatar] = useState(user?.photoURL);
+  const onAvatarChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (!user) return;
+    if (files && files.length === 1) {
+      const file = files[0];
+      const locationRef = ref(storage, `avatars/${user?.uid}`);
+      const result = await uploadBytes(locationRef, file);
+      const avatarUrl = await getDownloadURL(result.ref);
+      setAvatar(avatarUrl);
+      await updateProfile(user, {
+        photoURL: avatarUrl,
+      });
+    }
+  };
+  const fetchTweets = async () => {
+    const tweetQuery = query(
+      collection(db, "tweets"),
+      where("userId", "==", user?.uid),
+      limit(25)
+    );
+    const snapshot = await getDocs(tweetQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+    });
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
   return (
     <Wrapper>
-      <AvatarUpload>
-        <AvatarImg />
+      <AvatarUpload htmlFor="avatar">
+        {Boolean(avatar) ? (
+          <AvatarImg src={avatar} />
+        ) : (
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+          </svg>
+        )}
       </AvatarUpload>
-      <AvatarInput type="file" accept="image/*" />
+      <AvatarInput
+        onChange={onAvatarChange}
+        type="file"
+        id="avatar"
+        accept="image/*"
+      />
       <Name>{user?.displayName ?? "이름없음"}</Name>
     </Wrapper>
   );
